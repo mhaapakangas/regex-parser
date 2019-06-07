85fc266a9b4bf9737f13b8cd3d12acb80989c108
Maija Haapakangas <mhaapakangas@gmail.com>
2019-06-07 20:51:40 +0300

    Add method calls to warm up JVM for testing

--- src/main/java/Measurements.java
+++ src/main/java/Measurements.java
@@ -11,7 +11,6 @@ import java.util.regex.Pattern;
 
 public class Measurements {
     private static final Duration TIMEOUT = Duration.ofSeconds(10);
-
     private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(10);
 
     public static void main(String[] args) {
@@ -21,6 +20,13 @@ public class Measurements {
         }
 
         int limit = Integer.valueOf(args[0]);
+
+        // warm up the JVM
+        for (int i = 0; i < 10000; i++) {
+            runRegexParser("aaaaa", "aaaaa", true);
+            runJavaParser("aaaaa", "aaaaa", true);
+        }
+
         String regexp = "";
         String input = "";
         for (int i = 0; i < limit; i++) {
@@ -29,8 +35,8 @@ public class Measurements {
         }
 
         System.out.println("Regex: (a*)^n a^n, Input: a^n, n = " + limit);
-        runRegexParser(regexp, input);
-        runJavaParser(regexp, input);
+        runRegexParser(regexp, input, false);
+        runJavaParser(regexp, input, false);
 
         regexp = "";
         input = "";
@@ -39,25 +45,30 @@ public class Measurements {
             input += "a";
         }
 
-        System.out.println("Regex: a^n, Input: a^n, n = " + limit);
-        runRegexParser(regexp, input);
-        runJavaParser(regexp, input);
+        System.out.println("\nRegex: a^n, Input: a^n, n = " + limit);
+        runRegexParser(regexp, input, false);
+        runJavaParser(regexp, input, false);
 
         EXECUTOR.shutdown();
         System.exit(0);
     }
 
-    private static void runRegexParser(String regex, String input) {
+    private static void runRegexParser(String regex, String input, Boolean quiet) {
         final Future<Void> handler = EXECUTOR.submit(new Callable<Void>() {
             @Override
             public Void call() {
                 Instant start = Instant.now();
                 String prepared = InputConverter.addConcatenationChar(regex);
                 NFA nfa = NFABuilder.buildNFA(InputConverter.toPostfixNotation(prepared));
+                Instant time1 = Instant.now();
                 boolean result = RegexMatcher.match(nfa, input);
                 Instant finish = Instant.now();
                 long timeElapsed = Duration.between(start, finish).toMillis();
-                System.out.println("Regex-parser - is a match: " + result + ", time: " + timeElapsed + "ms");
+                if (!quiet) {
+                    System.out.println("NFA building: " + Duration.between(start, time1).toMillis() + "ms");
+                    System.out.println("Matching: " + Duration.between(time1, finish).toMillis() + "ms");
+                    System.out.println("Regex-parser - is a match: " + result + ", time: " + timeElapsed + "ms");
+                }
                 return null;
             }
         });
@@ -70,7 +81,7 @@ public class Measurements {
         }
     }
 
-    private static void runJavaParser(String regex, String input) {
+    private static void runJavaParser(String regex, String input, Boolean quiet) {
         final Future<Void> handlerJava = EXECUTOR.submit(new Callable<Void>() {
             @Override
             public Void call() {
@@ -80,7 +91,9 @@ public class Measurements {
                 boolean result = matcher.find();
                 Instant finish = Instant.now();
                 long timeJava = Duration.between(start, finish).toMillis();
-                System.out.println("Java regex - is a match: " + result + ", time: " + timeJava + "ms");
+                if (!quiet) {
+                    System.out.println("Java regex - is a match: " + result + ", time: " + timeJava + "ms");
+                }
                 return null;
             }
         });
